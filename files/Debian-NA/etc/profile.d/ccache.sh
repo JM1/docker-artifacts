e54b5fde5f4cbd4c219670a573a0f715f5f943d7
Jakob Meng <jakobmeng@web.de>
2021-10-10 10:24:52 +0200

    Fixed copyright headers, added modelines and dropped empty defaults

--- files/Debian-NA/etc/profile.d/ccache.sh
+++ files/Debian-NA/etc/profile.d/ccache.sh
@@ -1,4 +1,4 @@
-# (2020) WEARENOTALONE
+# 2020-2021 Jakob Meng, <jakobmeng@web.de>
 # Use Ccache, a fast C/C++ compiler cache, to speed up recompilation by caching the result
 # of previous compilations and detecting when the same compilation is being done again.
 # Ref.: man ccache
